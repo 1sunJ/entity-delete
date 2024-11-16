f1703c0213c23cd3c2453f7bdc4b2bd0bdff4802
1sunJ <won9619v@naver.com>
2024-11-16 13:34:00 +0900

    Feat: EntityDeleteConfig 추가

---
+++ src/main/java/study/delete_propagation/deletepropagation/EntityDeleteConfig.java
@@ -0,0 +1,32 @@
+package study.delete_propagation.deletepropagation;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.JpaRepository;
+import study.delete_propagation.deletepropagation.v1.EntityDeleteManagerV1;
+import study.delete_propagation.deletepropagation.v1.EntityRelationManagerV1;
+
+import java.util.Map;
+
+@Slf4j
+@Configuration
+@RequiredArgsConstructor
+public class EntityDeleteConfig {
+
+    private final Map<String, JpaRepository<?, ?>> repositoryMap;
+
+    @Bean
+    public EntityRelationManager entityRelations() {
+        return new EntityRelationManagerV1();
+    }
+
+    @Bean
+    public EntityDeleteManager entityDeleteManager() {
+        return new EntityDeleteManagerV1(
+                repositoryMap,
+                entityRelations());
+    }
+
+}
